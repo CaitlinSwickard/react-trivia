231fe7f8d20f4cb04a11fd1baea9a83c2f5e29e6
Caitlin Swickard <caitlin6029@gmail.com>
2024-01-13 15:58:27 -0700

    advance btn

--- src/App.tsx
+++ src/App.tsx
@@ -6,63 +6,61 @@ import { Questions } from "./types";
 import Reset from "./components/Reset";
 import StatBar from "./components/StatBar";
 
-
 const App = () => {
-    const allQuestions = questions as Questions;
+  const allQuestions = questions as Questions;
 
-    const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0); //state to move through questions
-    const [correctAnswers, setCorrectAnswers] = useState(0); // num of correct answers
-    const [incorrectAnswers, setIncorrectAnswers] = useState(0); // num of incorrect answers
+  const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [incorrectAnswers, setIncorrectAnswers] = useState(0);
 
-    const [waitingToAdvance, setWaitingToAdvance] = useState(false); //state to move to next question
+  const [waitingToAdvance, setWaitingToAdvance] = useState(false);
 
-    // updates score for stat bar
-    const onSubmit = (correct: boolean) => {
-        if (correct) setCorrectAnswers(correctAnswers + 1);
-        else setIncorrectAnswers(incorrectAnswers + 1);
+  const onSubmit = (correct: boolean) => {
+    if (correct) setCorrectAnswers(correctAnswers + 1);
+    else setIncorrectAnswers(incorrectAnswers + 1);
 
-        setWaitingToAdvance(true);
-    };
+    setWaitingToAdvance(true);
+  };
 
-    // update state to move to next question
-    const advance = () => {
-        setWaitingToAdvance(false);
-        setCurrentQuestionIdx(currentQuestionIdx + 1);
-    };
+  const advance = () => {
+    setWaitingToAdvance(false);
+    setCurrentQuestionIdx(currentQuestionIdx + 1);
+  };
 
-    // updates state to reset the game
-    const reset = () => {
-        setCurrentQuestionIdx(0);
-        setWaitingToAdvance(false);
-    };
+  const reset = () => {
+    setCurrentQuestionIdx(0);
+    setCorrectAnswers(0);
+    setIncorrectAnswers(0);
+    setWaitingToAdvance(false);
+  };
 
-    if (currentQuestionIdx >= allQuestions.questions.length)
-        return (
-            <Reset
-                totalQuestions={allQuestions.questions.length}
-                correctQuestions={correctAnswers}
-                onPress={reset}
-            />
-        );
-
+  if (currentQuestionIdx >= allQuestions.questions.length)
     return (
-        <div className="container">
-            <h1 className="title">2000's Trivia</h1>
-            <StatBar
-                currentQuestion={currentQuestionIdx + 1}
-                totalQuestions={allQuestions.questions.length}
-                correct={correctAnswers}
-                incorrect={incorrectAnswers}
-            />
-            <QuestionCard
-                question={allQuestions.questions[currentQuestionIdx]}
-                onSubmit={() => { }}
-            />
-            {waitingToAdvance && (
-                <button onClick={advance}>Next Question...</button>
-            )}
-        </div>
+      <Reset
+        totalQuestions={allQuestions.questions.length}
+        correctQuestions={correctAnswers}
+        onPress={reset}
+      />
     );
+
+  return (
+    <div>
+      <h1>2000's Trivia</h1>
+      <StatBar
+        currentQuestion={currentQuestionIdx + 1}
+        totalQuestions={allQuestions.questions.length}
+        correct={correctAnswers}
+        incorrect={incorrectAnswers}
+      />
+      <QuestionCard
+        question={allQuestions.questions[currentQuestionIdx]}
+        onSubmit={onSubmit}
+      />
+      {waitingToAdvance && (
+        <button onClick={advance}>Next Question...</button>
+      )}
+    </div>
+  );
 };
 
 export default App;
